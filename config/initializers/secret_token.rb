8797f3e09551b6239406de2893d58359beb8ac8d
lkvsky <kyle@lucovsky.com>
2013-02-22 15:34:47 -0800

    initial commit, set-up

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Coachella::Application.config.secret_token = 'a53178bc4af626d6a0dda09a0fcc8f959277fa5c97fdeb1ab4e11d02c2a76040d1021d428b0d539a26ef73288e507ebd1a0c2944f0b7964e935d66623d2d2ce7'
