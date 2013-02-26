0b84a28106a33705f6d8dee6a7a658a3abd9ec01
lkvsky <kyle@lucovsky.com>
2013-02-26 14:50:05 -0800

    some style and controller clean up

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,8 @@
 Coachella::Application.routes.draw do
   resources :playlists, :only => [:index, :create, :destroy]
   resources :bands, :only => [:index, :show]
-  resources :songs do
-    resources :song_likes, :only => [:create, :destroy], :as => :likes
-    resources :song_dislikes, :only => [:create, :destroy], :as => :dislikes
-  end
+  resources :song_likes, :only => [:create, :destroy], :as => :likes
+  resources :song_dislikes, :only => [:create, :destroy], :as => :dislikes
 
   root :to => "playlists#index"
 end
