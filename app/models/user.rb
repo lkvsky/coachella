564d37fdcca11ac5e7187049cce6ced76664cdda
lkvsky <kyle@lucovsky.com>
2013-03-05 09:44:55 -0800

    merge facebook accounts with existing accounts if signed up

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,6 +10,8 @@ class User < ActiveRecord::Base
   attr_accessible :email, :password, :password_confirmation, :remember_me,
                   :username, :provider, :uid, :access_token
 
+  validates :username, :presence => true
+
   has_many :playlists
   has_many :song_likes
   has_many :song_dislikes
@@ -21,25 +23,43 @@ class User < ActiveRecord::Base
     user = User.where(:provider => auth.provider, :uid => auth.uid).first
 
     unless user
-      user = User.create(
-          :provider => auth.provider,
-          :uid => auth.uid,
-          :email => auth.info.email,
-          :access_token => auth.credentials.token,
-          :password => Devise.friendly_token[0,20]
-        )
-      
-      unless guest_user.nil?
-        guest_user.transfer_associations(user)
-      end
+      if User.existing_user?(auth.info.email)
+        user = User.find_by_email(auth.info.email)
+        user.merge_existing_user_account(auth.provider, auth.uid, auth.credentials.token)
 
-      user
+        user
+      else
+        user = User.create(
+            :provider => auth.provider,
+            :uid => auth.uid,
+            :email => auth.info.email,
+            :access_token => auth.credentials.token,
+            :password => Devise.friendly_token[0,20]
+          )
+        
+        unless guest_user.nil?
+          guest_user.transfer_associations(user)
+        end
+
+        user
+      end
     end
+
+    user
   end
 
   def transfer_associations(new_user)
     self.playlists.each { |playlist| playlist.update_attributes(:user_id => new_user.id) }
     self.song_likes.each { |song_like| song_like.update_attributes(:user_id => new_user.id) }
     self.song_dislikes.each { |song_dislike| song_dislike.update_attributes(:user_id => new_user.id) }
+  end
+
+  def self.existing_user?(email)
+    return true if User.find_by_email(email)
+    false
+  end
+
+  def merge_existing_user_account(provider, uid, access_token)
+    self.update_attributes(:provider => provider, :uid => uid, :access_token => access_token)
   end
 end
